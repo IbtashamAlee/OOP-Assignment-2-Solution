5d2aa02ad2e7a7a4df02835a66f305e552a5f053
IbtaSham ALee <Aleejaan562@gmail.com>
2019-10-31 23:23:31 +0500

    Update Odometer.java

--- Question-1/Odometer.java
+++ Question-1/Odometer.java
@@ -1,28 +1,27 @@
+package com.company;
 
 public class Odometer {
-    private double fuel_efficiency;
+    private double fuel;
     private double miles;
 
     public Odometer() {
     }
 
-    public Odometer(double fuel_efficiency, double miles) {
-        this.fuel_efficiency = fuel_efficiency;
+    public Odometer(double fuel, double miles) {
+        this.fuel = fuel;
         this.miles = miles;
     }
 
     public void resetOdometer() {
         this.miles = 0;
-        this.fuel_efficiency = 0;
+        this.fuel = 0;
     }
 
     public void setMiles(double miles) {
         this.miles = miles;
     }
-
-    // miles per litter
     public void setFuelEfficency(double fuel) {
-        this.fuel_efficiency = fuel;
+        this.fuel = fuel;
     }
 
     public void milesDriven(double miles) {
@@ -31,8 +30,8 @@ public class Odometer {
 
     public double numberOfGallons() {
 
-        //According to US there is 3.78541 litters of fuel in one gallon
-        return (this.miles / this.fuel_efficiency) / 3.78541;
+        //3.78541 litters of fuel in one gallon
+        return (this.miles / this.fuel) / 3.78541;
     }
 
 }
